87affe2713a1950efd136fdfd6332b0f61862d8c
Sponkinho <swan.duval95@gmail.com>
2022-08-30 12:31:04 +0200

    Add register action

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,22 +2,52 @@
 
 namespace App\Controller;
 
-use Doctrine\ORM\EntityManager;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use DateTime;
+use App\Entity\User;
+use App\Form\RegisterFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+
 
 class RegisterController extends AbstractController
 {
     #[Route("/inscription", name: 'user_register', methods: ['GET', 'POST'])]
-    public function register(Request $request, EntityManager $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
+        # 1 - Instanciation
+        $user = new User();
 
+        # 2 - Création du formulaire + mécanisme d'auto-hydratation
+        $form = $this->createForm(RegisterFormType::class, $user)
+        ->handleRequest($request);
 
+        # 4 - Au clic du bouton 'Valider'
+        if($form->isSubmitted() && $form->isValid()){
+            #Set des propriétés qui ne sont pas dans le formulaire
+            $user->setCreatedAt(new DateTime());
+            $user->setUpdatedAt(new DateTime());
+            # La propriété "roles" est un array
+            $user->setRoles(['ROLE_USER']);
 
+            $user->setPassword(
+                $passwordHasher->hashPassword($user, $form->get('password')->getData())
+            );
 
+            $entityManager->persist($user);
+            $entityManager->flush();
 
+            return $this->redirectToRoute('default_home');
+        }
+
+        # 3 - Rendu de la vue Twig, avec le formulaire
+        return $this->render("register/form.html.twig", [
+            'form' => $form->createView() # createView() permet de générer le HTML pour l'affichage
+
+        ]);
     }
 }
--- src/Form/RegisterFormType.php
+++ src/Form/RegisterFormType.php
@@ -4,22 +4,46 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegisterFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('gender')
+            ->add('email', EmailType::class, [
+                "label" => "Email"
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe'
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('gender', ChoiceType::class, [
+                'label' => 'Civilité',
+                'expanded' => true,
+                'choices' => [
+                    'Homme' => 'h',
+                    'Femme' => 'f',
+                    'Non Binaire' => 'non binaire'
+                ],
+            ])
             ->add('submit', SubmitType::class,[
-                'label' => 'Valider'
+                'label' => 'Valider',
+                'validate' => false,
+                'attr' => [
+                    'class' => 'd-block mx-auto btn btn-primary col-3'
+                ]
             ])
         ;
     }
